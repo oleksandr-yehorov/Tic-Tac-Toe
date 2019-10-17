5cd31e359ac9eac55d478420f9d72f555d436f7d
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-18 00:54:56 +0300

    feat: checking for more turns

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -64,6 +64,17 @@ class TicTacToe {
         return this.winMark;
     }
 
+    noMoreTurns() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (!this.turns[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
     
 }
 
